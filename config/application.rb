a14b7d43b9ecc47d8244cd1a630e5194e77fd3f1
Connor Lay <connor.lay@me.com>
2015-07-16 15:35:03 -0600

    [refactor] use autoload! to load lib directory

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module SetApi
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.paths['lib'].autoload!
 
     config.api_only = false
     config.session_store :cookie_store
