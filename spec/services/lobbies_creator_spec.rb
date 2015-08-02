204eb8516754df6f202d8e647fc8e2619fdd2617
Connor Lay <connor.lay@me.com>
2015-08-02 11:51:46 -0600

    [fixed] running service specs multiple times resulting in rollback issues

--- spec/services/lobbies_creator_spec.rb
+++ spec/services/lobbies_creator_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Lobbies::Creator  do
 
-  pending "rollback issues here!"
-
   let(:creator) { Lobbies::Creator.new }
 
   describe "#create_new_lobby" do
     let(:user)  { create :user }
     let(:lobby) { creator.create_new_lobby(user) }
 
+    it "returns a new lobby" do
+      expect(lobby).to be_an_instance_of Lobby
+    end
     it "adds the user as a member" do
       expect(lobby.users.size).to eq 1
       expect(lobby.users).to include user
