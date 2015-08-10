5b2e2ee3227f36f5d37af06c00ba11271dd1cc50
Connor Lay <connor.lay@me.com>
2015-08-10 15:46:33 -0600

    [refactor] use private method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::UsersController < Api::V1::ApplicationController
 
   def show
-    user = User.find(params['id'])
     render json: user, serializer: UserSerializer
+  end
+
+  private
+
+  def user
+    @user ||= User.find(params[:id])
   end
 
 end
