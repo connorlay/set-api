ccd4730dd444fd988078c93dde6389685f2d325b
Connor Lay <connor.lay@me.com>
2015-08-11 14:18:29 -0600

    [remove] unused method

--- app/models/lobby.rb
+++ app/models/lobby.rb
@@ -4,10 +4,6 @@ class Lobby < ActiveRecord::Base
   has_many :users, through: :memberships
   has_one  :game
 
-  def add_user(user)
-    memberships.create user: user
-  end
-
   def has_user?(user)
     users.include? user
   end
