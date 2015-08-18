841a2134e118d307d8ee97f97b7e0ee95e23d3bd
Connor Lay <connor.lay@me.com>
2015-08-18 15:35:38 -0600

    [refactor] clean up validator

--- app/validators/set_validator.rb
+++ app/validators/set_validator.rb
@@ -1,30 +1,22 @@
 class SetValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, cards)
-    return false unless check_input(record, attribute, cards)
+
+    return false if cards.nil? || cards.empty?
+
     cards.first.attributes.keys.each do |attr|
       values = cards.map { |card| card.attributes[attr] }
       unless same_or_unique?(values)
         record.errors[attr] << "is not all the same or unique."
       end
     end
+
   end
 
   private
 
   def same_or_unique?(collection)
     collection.uniq.length == collection.length || collection.uniq.length == 1
-  end
-
-  def check_input(record, attribute, cards)
-    record.errors[attribute] << "is nil." if cards.nil?
-    if cards.try(:none?) { |card| card.respond_to? :attributes }
-      record.errors[attribute] << "elements must respond to :attributes"
-    end
-    if cards.try(:size).try(:zero?)
-      record.errors[attribute] << "cannot be empty."
-    end
-    record.errors.messages.empty?
   end
 
 end
