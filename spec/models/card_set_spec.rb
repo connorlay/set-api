51119c1fc31f55de322cb2605a5cf5435a07129c
Connor Lay <connor.lay@me.com>
2015-08-18 16:23:35 -0600

    [add] spec for valid sets

--- spec/models/card_set_spec.rb
+++ spec/models/card_set_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
 
 RSpec.describe CardSet, type: :model do
 
-  let(:card) { double("card", attributes: { color: :red }) }
+  let(:red)    { double("card", attributes: { color: :red    }) }
+  let(:yellow) { double("card", attributes: { color: :yellow }) }
+  let(:blue)   { double("card", attributes: { color: :blue   }) }
 
   it { is_expected.to validate_presence_of :cards }
 
+  it "validates a set" do
+    expect(CardSet.new(cards: [red, yellow, blue])).to       be_valid
+    expect(CardSet.new(cards: [red, red,    red])).to        be_valid
+    expect(CardSet.new(cards: [red, yellow, yellow])).not_to be_valid
+  end
+
   it "validates cards length" do
-    expect(CardSet.new(cards: [])).not_to             be_valid
-    expect(CardSet.new(cards: [card, card, card])).to be_valid
+    expect(CardSet.new(cards: [])).not_to              be_valid
+    expect(CardSet.new(cards: [red, yellow, blue])).to be_valid
   end
 
 end
